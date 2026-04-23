7d7e16425b03a4a9627aa72f103f0551d69a62ef
Csutaknimro <nimrodcsutak@gmail.com>
2026-04-23 14:14:38 +0200

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -2,6 +2,12 @@ input.onButtonPressed(Button.A, function () {
     led.plot(0, 0)
     basic.pause(500)
     led.unplot(0, 0)
+    basic.pause(500)
+    led.plot(0, 0)
+    basic.pause(500)
+    led.unplot(0, 0)
+    basic.pause(500)
+    led.plot(0, 0)
 })
 basic.forever(function () {
 	
